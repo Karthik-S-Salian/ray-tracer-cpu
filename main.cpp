88f897e4ff6be7f5be683813dae6302d81a07998
Karthik-S-Salian <karthikssalian5@gmail.com>
2023-11-23 20:56:20 +0530

    added vec3 point3 and color class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "vec3.h"
+#include "color.h"
 
 
 
@@ -11,17 +13,20 @@ int main(){
     for(int j=0;j<image_height;++j){
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i=0;i<image_width;++i){
-            auto r = double(i)/(image_width-1);
-            auto g = double(j)/(image_height-1);
-            auto b = 0;
+            // auto r = double(i)/(image_width-1);
+            // auto g = double(j)/(image_height-1);
+            // auto b = 0;
 
-            int ir = static_cast<int>(255.9999*r);
-            int ig = static_cast<int>(255.9999*g);
-            int ib = static_cast<int>(255.9999*b);
+            // int ir = static_cast<int>(255.9999*r);
+            // int ig = static_cast<int>(255.9999*g);
+            // int ib = static_cast<int>(255.9999*b);
 
-            std::cout<<ir<<' '<<ig<<' '<<ib<<'\n';
+            // std::cout<<ir<<' '<<ig<<' '<<ib<<'\n';
+
+            auto pixel_color =  color(double(i)/(image_width-1),double(j)/(image_height-1),0);
+            write_color(std::cout,pixel_color);
         }
     }
 
-    std::clog << "\rDone.\n";
+    std::clog << "\rDone              .\n";
 }
