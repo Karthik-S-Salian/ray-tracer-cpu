72cec5f28e084e35f6c2f1d630067af37e0b22cb
Karthik-S-Salian <karthikssalian5@gmail.com>
2023-11-29 11:14:31 +0530

    Using Gamma Correction for Accurate Color Intensity

--- camera.h
+++ camera.h
@@ -83,7 +83,7 @@ private:
         if (world.hit(r, interval(0.001, infinity), rec))
         {
             vec3 direction = rec.normal + random_unit_vector();
-            return .9 * ray_color(ray(rec.p, direction), depth-1,world);
+            return .5 * ray_color(ray(rec.p, direction), depth-1,world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
