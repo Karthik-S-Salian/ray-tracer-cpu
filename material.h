89d3b373324736ce2f716d3e3c84dfe7d9e08343
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-07-16 09:43:51 +0530

    checker texture

--- material.h
+++ material.h
@@ -4,6 +4,7 @@
 #include "rtweekend.h"
 #include "color.h"
 #include "hittable.h"
+#include "texture.h"
 
 class material {
   public:
@@ -16,8 +17,9 @@ class material {
 
 class lambertian : public material {
   public:
-    lambertian(const color& a) : albedo(a) {}
-
+    lambertian(const color& albedo): tex(make_shared<solid_color>(albedo)){}
+    lambertian(shared_ptr<texture> tex): tex(tex) {}
+    
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)const override {
         auto scatter_direction = rec.normal + random_unit_vector();
 
@@ -26,12 +28,12 @@ class lambertian : public material {
             scatter_direction = rec.normal;
 
         scattered = ray(rec.p, scatter_direction,r_in.time());
-        attenuation = albedo;
+        attenuation = tex->value(rec.u,rec.v,rec.p);
         return true;
     }
 
   private:
-    color albedo;
+    shared_ptr<texture> tex;
 };
 
 class metal : public material {
