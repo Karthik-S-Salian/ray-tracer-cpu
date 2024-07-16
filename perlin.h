dd2e3651cf78d5a3d8068fba9d53c6648bf7e0fa
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-07-16 17:56:25 +0530

    turbulence

--- perlin.h
+++ perlin.h
@@ -8,10 +8,10 @@ class perlin
 public:
     perlin()
     {
-        random_float = new double[point_count];
+        random_vec = new vec3[point_count];
         for (int i = 0; i < point_count; i++)
         {
-            random_float[i] = random_double();
+            random_vec[i] = unit_vector(vec3::random(-1,1));
         }
 
         perm_x = perlin_generate_perm();
@@ -21,7 +21,7 @@ public:
 
     ~perlin()
     {
-        delete[] random_float;
+        delete[] random_vec;
         delete[] perm_x;
         delete[] perm_y;
         delete[] perm_z;
@@ -32,30 +32,40 @@ public:
         auto u = p.x() - floor(p.x());
         auto v = p.y() - floor(p.y());
         auto w = p.z() - floor(p.z());
-        //Hermitian Smoothing
-        u = u * u * (3 - 2 * u);
-        v = v * v * (3 - 2 * v);
-        w = w * w * (3 - 2 * w);
 
         auto i = int(floor(p.x()));
         auto j = int(floor(p.y()));
         auto k = int(floor(p.z()));
 
-        double c[2][2][2];
+        vec3 c[2][2][2];
 
         for (int di = 0; di < 2; di++)
             for (int dj = 0; dj < 2; dj++)
                 for (int dk = 0; dk < 2; dk++)
-                    c[di][dj][dk] = random_float[perm_x[(i + di) & 255] ^
+                    c[di][dj][dk] = random_vec[perm_x[(i + di) & 255] ^
                                                  perm_y[(j + dj) & 255] ^
                                                  perm_z[(k + dk) & 255]];
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
+    }
+
+    double turb(const point3 &p,int depth) const {
+        auto accum =0.0;
+        auto temp_p = p;
+        auto weight = 1.0;
+
+        for(int i=0;i<depth;i++){
+            accum += weight*noise(temp_p);
+            temp_p*=2;
+            weight*=0.5;
+        }
+
+        return fabs(accum);
     }
 
 private:
     static const int point_count = 256;
-    double *random_float;
+    vec3 *random_vec;
     int *perm_x;
     int *perm_y;
     int *perm_z;
@@ -83,13 +93,19 @@ private:
         }
     }
 
-    static double trilinear_interp(double c[2][2][2], double u, double v, double w)
+    static double perlin_interp(const vec3 c[2][2][2], double u, double v, double w)
     {
+        auto uu = u*u*(3-2*u);
+        auto vv = v*v*(3-2*v);
+        auto ww = w*w*(3-2*w);
         auto accum = 0.0;
+
         for (int i = 0; i < 2; i++)
             for (int j = 0; j < 2; j++)
-                for (int k = 0; k < 2; k++)
-                    accum += (i * u + (1 - i) * (1 - u)) * (j * v + (1 - j) * (1 - v)) * (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+                for (int k = 0; k < 2; k++){
+                    vec3 weight_v(u-i,v-j,w-k);
+                    accum += (i * uu + (1 - i) * (1 - uu)) * (j * vv + (1 - j) * (1 - vv)) * (k * ww + (1 - k) * (1 - ww)) * dot(c[i][j][k],weight_v);
+                }
         return accum;
     }
 };
